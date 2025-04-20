7d4e55a4f6bc46faa0afb988a5c87705f88a65fa
Vladimir <v071487@gmail.com>
2025-04-21 01:21:24 +0300

    ver 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard (cards, deleteCard) {
+  const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = newCard.querySelector('.card__image');
+  const cardTitle = newCard.querySelector('.card__title');
+    
+  cardTitle.textContent = cards.name;
+  cardImage.src = cards.link;
+  cardImage.alt = cards.name;
+  
+
+  const cardDelButton = newCard.querySelector('.card__delete-button');
+  cardDelButton.addEventListener('click', () => {deleteCard(newCard)});
+
+  return newCard;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard (newCard) {
+  newCard.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(item => {
+  const newCard = createCard(item, deleteCard);
+  placesList.append(newCard);
+})
